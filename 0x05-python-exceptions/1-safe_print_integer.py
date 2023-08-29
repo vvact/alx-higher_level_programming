3b9dbe609008380c65c803f32f6a8b17611f2127
Victor Midega <vbm7752@gmail.com>
2023-08-29 02:26:13 -0700

    new commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if value >= 0 or value < 0:
-            print("{:d}" .format(value))
+            print("{:d}".format(value))
             digit_it_is = True
     except Exception:
         digit_it_is = False
