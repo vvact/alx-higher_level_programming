b02c451b3309a1fc10e9f0b47d1cee863c7ccb6b
Victor Midega <vbm7752@gmail.com>
2023-08-29 14:41:45 -0700

    function to print integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list= [],0=x):
-    real_num_of_elements = 0
-    for w in range(x)
+def safe_print_list_integers(my_list=[], x=0):
+    real_no_of_elems = 0
+    for w in range(x):
         try:
             print("{:d}".format(my_list[w]), end="")
-            real_num_of_elements += 1
+            real_no_of_elems += 1
         except(ValueError, TypeError):
             pass
     print()
-    print real_num_of_elements
+    return real_no_of_elems
