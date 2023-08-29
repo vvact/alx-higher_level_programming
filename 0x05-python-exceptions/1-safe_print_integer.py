f9659619358ba1fe8675cfe0b8f661446c0b0b85
Victor Midega <vbm7752@gmail.com>
2023-08-29 02:21:51 -0700

    function that prints an integer with formated specifer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,6 @@ def safe_print_integer(value):
         if value >= 0 or value < 0:
             print("{:d}" .format(value))
             digit_it_is = True
-
     except Exception:
         digit_it_is = False
     return  digit_it_is
