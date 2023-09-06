2f7a92e366a0ea8cd0af73f3d423043ba0a6c36f
Victor Midega <vbm7752@gmail.com>
2023-09-06 14:34:32 -0700

    new commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle():
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__h
+        return 2 * (self.__width + self.__height)
